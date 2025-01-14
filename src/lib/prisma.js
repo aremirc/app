8024a3d146923749c4d92e510850cb293f84821f
aremirc <aramirezcarhuancho@gmail.com>
2025-01-14 14:46:51 -0500

    Correction tests

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,4 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  // En desarrollo, puede ser útil tener una nueva instancia en cada solicitud
+  // para evitar la re-conexión innecesaria con la base de datos.
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 module.exports = prisma;
